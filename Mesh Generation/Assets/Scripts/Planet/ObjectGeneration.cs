b517d4a622372db51e18c96eed2be977a00cf41b
GMcMichael <gmcmichael0717@gmail.com>
2021-05-16 23:53:43 -0700

    Added basic resources
    Added basic resource prefabs and planet spawning

--- Mesh Generation/Assets/Scripts/Planet/ObjectGeneration.cs
+++ Mesh Generation/Assets/Scripts/Planet/ObjectGeneration.cs
@@ -20,11 +20,17 @@ public class ObjectGeneration : MonoBehaviour
     private int[] ObjectTypes;
 
 
+    private enum ResourceFertility {barren = 1, scarce, normal, rich, plentiful};//How many resource nodes will be in one clump of nodes
+    [SerializeField]
+    private ResourceFertility resourceFertility;
     [SerializeField]
     private int resourceDensity;
     private GameObject[] resources;
     private Vector3[] resourcePositions;
     private int[] resourceTypes;
+    private int minResourceSpacing = 30;
+    [SerializeField]
+    private int resourceRotation = 2;
 
     void Awake()
     {
@@ -35,8 +41,8 @@ public class ObjectGeneration : MonoBehaviour
     {
         ObjectPositions = new Vector3[objectDensity];
         ObjectTypes = new int[objectDensity];
-        resourcePositions = new Vector3[resourceDensity];
-        resourceTypes = new int[resourceDensity];
+        resourcePositions = new Vector3[resourceDensity*(int)resourceFertility];
+        resourceTypes = new int[resourceDensity*(int)resourceFertility];
     }
 
     public void Generate()
@@ -61,22 +67,59 @@ public class ObjectGeneration : MonoBehaviour
         }
     }
 
-    private void PlanResources() {//spawns resources on random land vertex
+    private void PlanResources() {//spawns resource groups on random land vertex
+        int index = 0;
         for (int i = 0; i < resourceDensity; i++)
         {
-            resourcePositions[i] = meshVertices[Random.Range(0, meshVertices.Length - 1)];
-            resourceTypes[i] = Random.Range(0, ResourcePrefabs.Count);
+            Vector3 newPos = GetNewResourcePosition(i*(int)resourceFertility);//makes sure the groups are far enough apart
+            int resourceType = Random.Range(0, ResourcePrefabs.Count);
+            //Vector3[] nextPoints = NearestPoints(newPos);
+            for (int j = 0; j < (int)resourceFertility; j++)
+            {
+                //get direction from planet to newPos
+                Vector3 Dir = transform.TransformPoint(newPos) - transform.position;
+                //rotate the vector by a random x and y
+                float[] angles = GetRotations();
+                Vector3 rotatedPos = Quaternion.Euler(angles[0], angles[1], 0) * Dir;
+                //add vector to planet pos with an offset
+                Vector3 nextPos = transform.position + rotatedPos*1.1f;
+                //Raycast to planet to get posititon
+                RaycastHit hit;
+                Ray ray = new Ray(nextPos, -Dir);
+                if(GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity)) nextPos = transform.InverseTransformPoint(hit.point);
+                resourcePositions[index] = nextPos;
+                resourceTypes[index] = resourceType;
+                index++;
+            }
         }
     }
 
-    private void InstantiateObjects()//Need to call this when I get to a planet
+    private float[] GetRotations() {
+        float[] angles = new float[2];
+        angles[0] = Random.Range(-resourceRotation*1f, resourceRotation*1f);
+        angles[1] = Random.Range(-resourceRotation*1f, resourceRotation*1f);
+        if(angles[0] == 0 && angles[1] == 0) angles = GetRotations();
+        return angles;
+    }
+
+    private Vector3 GetNewResourcePosition(int index) {
+        Vector3 newPos = meshVertices[Random.Range(0, meshVertices.Length - 1)];
+        for (int i = 0; i < index; i++)
+        {
+            if(Vector3.Distance(newPos, resourcePositions[i]) < minResourceSpacing)
+                newPos = GetNewResourcePosition(index);
+        }
+        return newPos;
+    }
+
+    private void InstantiateObjects()//Could call this when I get to a planet
     {
-        objects = new GameObject[objectDensity+resourceDensity];
+        objects = new GameObject[objectDensity+(resourceDensity*(int)resourceFertility)];
         for (int i = 0; i < objectDensity; i++)
         {
             objects[i] = SpawnObject(0, ObjectPositions[i], ObjectTypes[i]);
         }
-        for (int i = 0; i < resourceDensity; i++)
+        for (int i = 0; i < (resourceDensity*(int)resourceFertility); i++)
         {
             objects[objectDensity+i] = SpawnObject(1, resourcePositions[i], resourceTypes[i]);
         }
@@ -93,11 +136,11 @@ public class ObjectGeneration : MonoBehaviour
                 prefab = ResourcePrefabs[type];
                 break;
         }
-        spawnPosition = mesh.transform.TransformPoint(spawnPosition);
-        return Instantiate(prefab, spawnPosition, Quaternion.FromToRotation(transform.up, -(transform.position - spawnPosition)), mesh.transform);
+        spawnPosition = transform.TransformPoint(spawnPosition);
+        return Instantiate(prefab, spawnPosition, Quaternion.FromToRotation(transform.up, -(transform.position - spawnPosition)), transform);
     }
 
-    private void DestroyObjects()//need to call this when I leave a planet
+    private void DestroyObjects()//Could call this when I leave a planet
     {
         for (int i = 0; i < objects.Length; i++)
         {
